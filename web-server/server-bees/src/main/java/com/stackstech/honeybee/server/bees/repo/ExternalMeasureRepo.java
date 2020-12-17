acdfeb9bd7e25c00636308cde9a6002f70c7e4ae
william <william@qike.club>
2020-12-17 13:40:03 +0800

    update server-bees javadoc and package struct v1

--- web-server/server-bees/src/main/java/com/stackstech/honeybee/server/bees/repo/ExternalMeasureRepo.java
+++
@@ -1,7 +0,0 @@
-package com.stackstech.honeybee.server.bees.repo;
-
-
-import com.stackstech.honeybee.server.bees.entity.ExternalMeasure;
-
-public interface ExternalMeasureRepo extends MeasureRepo<ExternalMeasure> {
-}
--- web-server/server-bees/src/main/java/com/stackstech/honeybee/server/bees/repo/GriffinMeasureRepo.java
+++
@@ -1,7 +0,0 @@
-package com.stackstech.honeybee.server.bees.repo;
-
-
-import com.stackstech.honeybee.server.bees.entity.GriffinMeasure;
-
-public interface GriffinMeasureRepo extends MeasureRepo<GriffinMeasure> {
-}
