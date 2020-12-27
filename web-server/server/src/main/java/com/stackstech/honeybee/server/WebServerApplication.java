6f1a86f404816be25511df6f721d15f31a4c509d
william <william@qike.club>
2020-12-27 21:52:32 +0800

    project refactoring update v1.4.3

--- web-server/server/src/main/java/com/stackstech/honeybee/server/WebServerApplication.java
+++ web-server/server/src/main/java/com/stackstech/honeybee/server/WebServerApplication.java
@@ -10,6 +10,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
+/**
+ * @author william
+ */
 @SpringBootApplication
 @EnableAutoConfiguration
 @ImportResource({"classpath*:shiro.xml"})
