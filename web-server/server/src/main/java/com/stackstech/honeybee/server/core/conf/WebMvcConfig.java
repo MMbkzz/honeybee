19ac4970d9cca235a167a8bb8e47075eef7407c4
william <william@qike.club>
2021-01-18 20:33:08 +0800

    update swagger version to 3.0.0

--- web-server/server/src/main/java/com/stackstech/honeybee/server/core/conf/WebMvcConfig.java
+++ web-server/server/src/main/java/com/stackstech/honeybee/server/core/conf/WebMvcConfig.java
@@ -18,11 +18,9 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+                .resourceChain(false);
         super.addResourceHandlers(registry);
     }
 
