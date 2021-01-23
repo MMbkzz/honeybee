8d176adb024845398856a97c59bbc94a94968940
william <william@qike.club>
2021-01-23 19:13:25 +0800

    update: update module struct v2.5

--- honeybee-cloud-modules/bees/src/main/scala/com/stackstech/honeybee/bees/step/builder/dsl/expr/AliasableExpr.scala
+++
@@ -1,8 +0,0 @@
-
-package com.stackstech.honeybee.bees.step.builder.dsl.expr
-
-trait AliasableExpr extends Expr {
-
-  def alias: Option[String]
-
-}
--- honeybee-cloud-modules/bees/src/main/scala/com/stackstech/honeybee/bees/step/builder/dsl/expr/ExprTag.scala
+++
@@ -1,6 +0,0 @@
-
-package com.stackstech.honeybee.bees.step.builder.dsl.expr
-
-trait ExprTag { this: Expr =>
-  var tag: String = ""
-}
