88c5e49cb4a9d186eedefb69b06f9ea8998d8f3d
william <william@qike.club>
2021-01-23 14:46:20 +0800

    update: update project struct v1.4

--- honeybee-cloud-server/system-server/src/main/java/com/stackstech/honeybee/server/core/entity/AccountEntity.java
+++ honeybee-cloud-server/system-server/src/main/java/com/stackstech/honeybee/server/core/entity/AccountEntity.java
@@ -2,7 +2,10 @@ package com.stackstech.honeybee.server.core.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.stackstech.honeybee.server.core.enums.EntityStatusType;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -31,4 +34,19 @@ public class AccountEntity extends DataEntity<AccountEntity> {
 
     private String desc;
 
+    @Override
+    public AccountEntity build(Long ownerId) {
+        this.owner = ownerId;
+        this.status = EntityStatusType.ENABLE.getStatus();
+        this.createtime = new Date();
+        this.updatetime = new Date();
+        return this;
+    }
+
+    @Override
+    public AccountEntity update(Long ownerId) {
+        this.owner = ownerId;
+        this.updatetime = new Date();
+        return this;
+    }
 }
