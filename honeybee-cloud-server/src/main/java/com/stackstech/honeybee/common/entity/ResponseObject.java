ad6fc666b21e253560dc3afbc22a743fc29bc6d1
william <william@qike.club>
2021-02-16 16:05:47 +0800

    update: update response object java doc

--- honeybee-cloud-server/src/main/java/com/stackstech/honeybee/common/entity/ResponseObject.java
+++ honeybee-cloud-server/src/main/java/com/stackstech/honeybee/common/entity/ResponseObject.java
@@ -8,6 +8,12 @@ import lombok.NoArgsConstructor;
 import javax.annotation.Nullable;
 import java.util.Map;
 
+/**
+ * Response object
+ *
+ * @author william
+ * @since 1.0
+ */
 @Data
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -20,6 +26,14 @@ public class ResponseObject {
     private Integer total;
     private Object result;
 
+    /**
+     * get success response object
+     *
+     * @param message message
+     * @param result  data
+     * @param total   data size
+     * @return ResponseObject
+     */
     public ResponseObject success(@Nullable String message, @Nullable Object result, @Nullable Integer total) {
         this.status = StatusCode.SUCCESS;
         this.message = message;
@@ -28,22 +42,57 @@ public class ResponseObject {
         return this;
     }
 
+    /**
+     * get success response object
+     *
+     * @param message message
+     * @param result  data
+     * @return ResponseObject
+     */
     public ResponseObject success(String message, Object result) {
         return success(message, result, null);
     }
 
+    /**
+     * get success response object
+     *
+     * @param result data
+     * @param total  data size
+     * @return ResponseObject
+     */
     public ResponseObject success(Object result, Integer total) {
         return success(StatusCode.SUCCESS.getMessage(), result, total);
     }
 
+    /**
+     * get success response object
+     *
+     * @param message message
+     * @return ResponseObject
+     */
     public ResponseObject success(String message) {
         return success(message, null, null);
     }
 
+    /**
+     * get success response object
+     *
+     * @param result data
+     * @return ResponseObject
+     */
     public ResponseObject success(Object result) {
         return success(StatusCode.SUCCESS.getMessage(), result);
     }
 
+    /**
+     * get failed response object
+     *
+     * @param status   status
+     * @param messages multi line error message
+     * @param error    error count
+     * @return ResponseObject
+     * @see StatusCode
+     */
     public ResponseObject failed(StatusCode status, Map<String, Object> messages, Integer error) {
         this.status = status;
         this.messages = messages;
@@ -51,16 +100,35 @@ public class ResponseObject {
         return this;
     }
 
+    /**
+     * get failed response object
+     *
+     * @param status  status
+     * @param message message
+     * @return ResponseObject
+     * @see StatusCode
+     */
     public ResponseObject failed(StatusCode status, String message) {
         this.status = status;
         this.message = message;
         return this;
     }
 
+    /**
+     * get default failed response object
+     *
+     * @param message message
+     * @return ResponseObject
+     */
     public ResponseObject failed(String message) {
         return failed(StatusCode.FAILED, message);
     }
 
+    /**
+     * build a new response object
+     *
+     * @return ResponseObject
+     */
     public static ResponseObject build() {
         return new ResponseObject();
     }
